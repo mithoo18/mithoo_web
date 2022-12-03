b9324f9cf9aa4165f540a1ad76241df03364914c
mithoo18 <devanshu18g@gmail.com>
2022-12-03 14:14:12 +0530

    changes

---
+++ lib/tabs/about_tab.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+import 'package:mithoo_web/config/assets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+
+class AboutTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child : Center(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom : 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: Image.asset(Assets.avatar).image,
+              ),
+              
+              SizedBox(
+                height: 20,
+              ),
+
+              Text(
+                'Devanshu Gupta',
+                textScaleFactor: 4,
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+
+              Text(
+                'Android, Fluuter, Cricket, Music,\nLikes Traveling.',
+                style: Theme.of(context).textTheme.caption,
+                textScaleFactor: 2,
+                textAlign: TextAlign.center,
+              ),
+
+              SizedBox(
+                height: 40,
+                ),
+
+             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                                  FlatButton.icon(
+                    icon: SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: Image.asset(Assets.github)),
+                    label: Text('Github'),
+                    onPressed: () => html.window
+                        .open(Constants.PROFILE_GITHUB, 'adityadroid'),
+                  ),
+                  FlatButton.icon(
+                    icon: SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: Image.asset(Assets.twitter)),
+                    label: Text('Twitter'),
+                    onPressed: () => html.window
+                        .open(Constants.PROFILE_TWITTER, 'adityadroid'),
+                  ),
+                  FlatButton.icon(
+                    icon: SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: Image.asset(
+                            ThemeSwitcher.of(context).isDarkModeOn
+                                ? Assets.medium
+                                : Assets.medium_light)),
+                    label: Text('Medium'),
+                    onPressed: () => html.window
+                        .open(Constants.PROFILE_MEDIUM, 'adityadroid'),
+                  )
+              ],
+             ),
+              Row()
+
+
+            ],
+          ),
+        ),
+      )
+
+    );
+  }
+}
